90c3cb6d7764449659dee52d2dfe54e4ca393808
tmkimm <xoals1417@gmail.com>
2020-10-01 16:37:28 +0900

    semicolon

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose')
+const bodyParser =  require('body-parser')
 
-mongoose.connect('mongodb+srv://tmkimm:1417pp@mongo.kk3eq.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~~~')
+  res.send('Hello World!!!!!! :)')
+})
+
+app.post('/register', (req, res) => {
+  console.log(`req.body : ${req.body}`)
+  const user = new User(req.body)
+  console.log(`new User : ${user}`)
+  user.save((err, data) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({ success: true})
+  })
 })
 
 app.listen(port, () => {
