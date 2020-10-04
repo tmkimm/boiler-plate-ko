49351486cca988650f6e192423b88d9c35685938
tmkimm <xoals1417@gmail.com>
2020-10-04 22:24:02 +0900

    JWT 적용

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose')
 const bodyParser =  require('body-parser')
+const cookieParser =  require('cookie-parser')
 const config = require('./config/key');
 const { User } = require('./models/User');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
@@ -18,7 +20,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!!!!!! :)')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   const user = new User(req.body)
   user.save((err, data) => {
     if(err) return res.json({ success: false, err})
@@ -26,12 +28,41 @@ app.post('/register', (req, res) => {
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   // 요청된 이메일이 DB에 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "아이디 또는 비밀번호가 잘못되었습니다."
+      })
+    }
+    // DB에 있다면 비밀번호가 맞는지 확인한다.
+    user.comparePassword(req.body.password, function(err, isMatch) {
+      if(!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 잘못되었습니다."
+        })
+      }
 
-  // DB에 있다면 비밀번호가 맞는지 확인한다.
+      // 비밀번호가 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
 
-  // 비밀번호가 맞다면 토큰을 생성하기.
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true,
+          userId: user._id
+        })
+      }) 
+    })
+  })
+
+
+ 
+  
 
 })
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -49,6 +50,24 @@ userSchema.pre('save', function( next ){
         next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // plainPassword: 입력한 비밀번호
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    const user = this
+    const token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return (err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
