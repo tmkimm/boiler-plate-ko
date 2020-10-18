9bd39e50298e32229ccd881969836ebfc80f9928
tmkimm <xoals1417@gmail.com>
2020-10-18 11:50:01 +0900

    로그인 페이지

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LoginPage() {
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHahdler = (e) => {
+        setEmail(e.target.value)
+    }
+    const onPasswordHahdler = (e) => {
+        setPassword(e.target.value)
+    }
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+    }
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            <form style={{ display:'flex', flexDirection: 'column'}}
+                  onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHahdler}></input>
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHahdler}></input>         
+                <br />
+                <button>Login</button>       
+            </form>
         </div>
     )
 }
